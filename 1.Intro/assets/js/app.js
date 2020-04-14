f42fd87ca4f2b63345e0f70e7ee517c9751b0c9b
kamranAeff <kamran_aeff@mail.ru>
2020-04-14 19:43:23 +0400

    Lesson-1

---
+++ 1.Intro/assets/js/app.js
@@ -0,0 +1,33 @@
+//sdsadasdasdasdasdasd asdasdas 
+
+/*dewewe
+we
+we
+we
+ 
+we
+we
+we
+we
+we
+*/
+
+// alert('Salam Javascript!');
+
+// document.getElementById('welcome').style.color = 'red';
+
+
+// single line
+/*
+multiline
+multiline
+multiline
+multiline
+*/
+
+
+alert('Hi');
+alert('P313');
+
+
+
---
+++ 1.Intro/assets/js/variables.js
@@ -0,0 +1,82 @@
+let name = 'Kamran';
+let profession = "Programmer";
+let yashi = 80;
+let herbiMukellefiyyet = false;  /*true: he,  false: yox */
+let dogumTarixi = Date();
+
+/*
+1
+soz
+
+Insan
+
+{
+    adi
+    soyadi
+    yashi
+    ataadi
+    boyu
+    cekisi
+    .
+    .
+    .
+
+}
+
+
+String    - metnleri yadda saxlamaq ucun
+Number    - reqemleri,ededleri yada saxlamaq ucun
+Boolean   - he yox mentiqi cavablari saxlamaq ucun
+Array     - coxluq halinda melumat saxlamaq ucun
+Object    - murekkeb tipleri yadda saxlamaq ucun,, Insan, mashin, Komputer,telefon
+
+
+function  - funksiyalari yadda saxlamaq ucun
+*/
+
+// let a = 'Bu bir atalar sozudur, gercek heyatla elaqesi yoxdur!';
+// let b = 8; //'sekkiz';
+
+// let $c = true;
+
+
+// let yash; //declaration
+
+
+// yash = 13; //assignment
+
+// yash = 18;  // declaration and assignment
+
+// case sensetive
+let c = 4;
+let C = 5;
+const pi = 3.14;
+
+/*
+var
+
+es6
+let
+const
+*/
+
+// let c = 6;
+
+// pi = 3;
+//c = 55;
+
+console.log(c);
+console.log(C);
+
+console.log('P313');
+
+
+
+
+
+
+
+
+
+
+
