a40e977f30351fe7f5c82b734135cddb578483ac
kamranAeff <kamran_aeff@mail.ru>
2020-04-16 22:24:17 +0400

    Lesson-4

---
+++ 04.PopupBoxes/assets/js/app.js
@@ -0,0 +1,85 @@
+/*
+Alert    --- istifadeciye bir mesaj gostere bilirik, Salam deye bilerik
+Prompt   --- istifadeci bir sey yazib bize gondere bilir
+Confirm  --- raziliq aliriq, razisan yoxsa yox?
+*/
+
+// let a = 5;  //sm
+
+// let area = a * a;
+
+// alert('kvadratin sahesi: '+area);
+// console.log(area);
+
+
+// alert('Sisteme daxil olmaq')
+// let password = prompt('Sifrenizi daxil edin');
+// let password = prompt('Sifrenizi daxil edin',3);
+
+// if(password == '123')
+// {
+//     alert('Xosh gelmisiniz');
+// }
+// else
+// {    
+//     alert('Sifreniz sehvdir');
+// }
+
+// let cavab = confirm('Yeni versiya movcuddur. IOS 9.2.0.Yenilemek isteyirsiniz?');
+
+// // if (cavab == true) {  //true == true  //true  ,false
+// //     document.write('sistem yenilenir.zehmet olmasa gozleyin... (3%)');
+// // }
+// // else {
+// //     document.write('sistem yenilikleri icra olunmadi');
+// // }
+
+
+// if (cavab) {  //true == true  //true  ,false
+//     document.write('sistem yenilenir.zehmet olmasa gozleyin... (3%)');
+// }
+// else {
+//     document.write('sistem yenilikleri icra olunmadi');
+// }
+
+// console.log(cavab);
+
+
+
+/*
+istifadeci 3reqemli eded daxil edir
+2ci reqemi cutdurse 'Qazandiniz'
+yox 2ci reqemi tekdirse 'Bir daha sinayin'  mesaj cixarmali
+*/
+
+// let value = prompt('3reqemli eded daxil edin');  // :string
+
+// // value = '2';
+
+// let eded = parseInt(value);  // '2' =>>> 2 
+
+// // 123   => 12
+
+// let lN = eded % 10;
+
+// eded = (eded - lN) / 10;  //12
+
+// eded = eded % 10;  // 2
+
+// // .toString()
+// // .toFixed()
+
+// // String()
+// // Number()
+// if(eded % 2 ==0) //cutdur
+// {
+//     alert('Qazandiniz!');
+// }
+// else
+// {
+//     alert('bir daha sinayin!');
+// }
+
+
+
+
---
+++ 05.Loops/assets/js/app.js
@@ -0,0 +1,145 @@
+
+// let a = 2;
+// let b = 3;
+
+////xetti alqoritm
+//let c = a + b;
+
+
+// let c;
+
+////budaqlanan alqoritm
+// if (b != 0) {
+//     c = a / b;
+// }
+// else {
+//     alert('0-a bolmek olmaz!');
+// }
+
+//
+
+
+//dovru aqloritmler
+// console.log(1);
+
+/*
+for
+while
+do..while
+foreach
+for..in
+for..of
+*/
+
+// let a = 12345;
+// let b;
+
+// b = a % 10;   // 5
+// a = (a - b) / 10;
+
+
+// b = a % 10;   // 4
+// a = (a - b) / 10;
+
+// b = a % 10;   // 3
+// a = (a - b) / 10;
+
+// 0
+// 1
+// 2
+// 3
+
+
+// 999
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+
+// 5 
+
+//[20;50]   cut ededleri cap et
+// let a= 3;
+// if(a%2==0)
+// {
+
+// }
+// else{
+
+// }
+
+// for (let i = 1; i <= 100; i++) {
+
+//     if (i % 5 == 0 && i % 3 == 0) {
+//         console.log(i);
+//     }
+
+// }
+
+/*
+10 - 50 ededlerin arasindan sonuncu rəqəmi 
+3-yə tam bölünürsə hemin ededleri cap etmek.
+*/
+
+// let sonuncu;
+// for (let a = 10; a <= 50; a++) {
+//     sonuncu = a % 10;
+
+//     if (sonuncu != 0 && sonuncu % 3 == 0) {
+//         console.log(a);
+//     }
+
+// }
+
+
+/*
+5.
+butun 2reqemli ededlerin icinden
+ reqemleri bir birine beraber olanlari secin
+
+*/
+//10-99
+
+
+/*
+
+1-100-ə kimi ədədləri hasilini tapmaq
+
+
+*/
+
+let hasil = 1;
+/*
+1    1*1
+2    1*2
+3    2*3
+4    6*4
+5    24*5
+*/
+
+/*
+a,b,c
+*/
+
+/*
+[20-90]
+
+butun cut ededlerin hasilini tapmaq
+
+*/
+
+for (let i = 1; i <= 5; i++) {
+    hasil = hasil * i;
+}
+
+console.log(hasil);
+
+
+
+
+
+
+
